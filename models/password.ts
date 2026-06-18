d3e9713d29f5071d7856ef7df2aa748628a5af8e
Kri777-oss <krishdhaked777@gmail.com>
2026-06-18 22:34:36 +0530

    model password creation for password schema

---
+++ models/password.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, models, model } from "mongoose";
+
+const PasswordSchema = new Schema(
+  {
+    userId: { type: String, required: true }, // Clerk user ID
+    title: { type: String, required: true },
+    category: { type: String }, // Wi-Fi, Credit Card, Code, etc.
+    username: String,
+    password: String,
+    notes: String,
+
+    // ✅ Add these for credit card category
+    cardNumber: String,
+    cvv: String,
+    expiry: String,
+  },
+  { timestamps: true }
+);
+
+export default models.Password || model("Password", PasswordSchema);
